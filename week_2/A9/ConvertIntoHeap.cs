d04a3bcae81ebe7c765838334becee50a7f8d610
nargeshm99 <nargeshm99@outlook.com>
2019-07-23 21:36:38 +0430

    Do week two assignments
    second week assignments

---
+++ week_2/A9/ConvertIntoHeap.cs
@@ -0,0 +1,54 @@
+﻿using TestCommon;
+using System;
+using System.Collections.Generic;
+
+namespace A9
+{
+    public class ConvertIntoHeap : Processor
+    {
+        public ConvertIntoHeap(string testDataName) : base(testDataName) { }
+
+        public override string Process(string inStr) =>
+            TestTools.Process(inStr, (Func<long[], Tuple<long, long>[]>)Solve);
+
+
+        public Tuple<long, long>[] Solve(
+            long[] array)
+        {
+            List<Tuple<long, long>> outPut = new List<Tuple<long, long>>();
+            int n = (array.Length-1) / 2;
+            for(int i = n;i>=0;i--)
+            {   
+                SiftDown(i,array,outPut);
+            }
+            return outPut.ToArray();
+ 
+        }
+
+        private void SiftDown(int i, long[] array, List<Tuple<long, long>> outPut)
+        {
+            int index = i;
+            int l = (2 * i) + 1;
+            if (l < array.Length && array[l] < array[index])
+                index = l;
+            int r = (2 * i) + 2;
+            if (r < array.Length && array[r] < array[index])
+                index = r;
+            if (i != index)
+            {
+                outPut.Add(new Tuple<long, long>(i, index));
+                swap(i, index, array);
+                SiftDown(index, array,outPut);
+            }
+        }
+
+        
+        private void swap(int i, int index, long[] array)
+        {
+            long a = array[i];
+            array[i] = array[index];
+            array[index] = a;
+        }
+    }
+
+}
---
+++ week_2/A9/MergingTables.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using TestCommon;
+
+namespace A9
+{
+    public class MergingTables : Processor
+    {
+        public MergingTables(string testDataName) : base(testDataName) { }
+
+        public override string Process(string inStr) =>
+            TestTools.Process(inStr, (Func<long[], long[], long[], long[]>)Solve);
+
+        public long[] Solve(long[] tableSizes, long[] sourceTables, long[] targetTables)
+        {
+            List<long> output = new List<long>(sourceTables.Length);
+            long[] parents = new long[tableSizes.Length];
+            for (int i = 0; i < sourceTables.Length; i++)
+            {
+                long indexSourceParent = sourceTables[i] - 1;
+                while (parents[indexSourceParent] != 0)
+                    indexSourceParent = parents[indexSourceParent];
+
+                long indexTargetParent = targetTables[i] - 1;
+                while (parents[indexTargetParent] != 0)
+                    indexTargetParent = parents[indexTargetParent];
+
+                if(indexSourceParent != indexTargetParent)
+                {
+                    parents[indexSourceParent] = indexTargetParent;
+                    tableSizes[indexTargetParent] += tableSizes[indexSourceParent];
+                }
+
+                output.Add(tableSizes.Max());
+            }
+            return output.ToArray();
+        }
+    }
+}
